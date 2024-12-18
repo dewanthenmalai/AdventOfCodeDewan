3507777fae51f639aadf8e5b29078286637e4ae9
Dewan Thenmalai <dewan.thenmalai@gmail.com>
2024-12-18 10:15:08 -0600

    changes to Day 18

--- 2024/Day18/Puzzle2.py
+++ 2024/Day18/Puzzle2.py
@@ -28,42 +28,13 @@ def tuple_add(a,b):
         raise ValueError("Lengths are not equal")
     return tuple(i+j for i,j in zip(a,b))
 
-def manhattan(a, b):
-    if len(a) != len(b):
-        raise ValueError("Lengths are not equal")
-    return sum(abs(i-j) for i,j in zip(a,b))
-
-def a_star(start, goal, edges, h):
-    open = {start}
-    prev = defaultdict(lambda: None)
-    gScore = defaultdict(lambda: math.inf)
-    gScore[start] = 0
-    fScore = defaultdict(lambda: math.inf)
-    fScore[start] = h(start,goal)
-    while open:
-        current = min(open,key=lambda v: fScore[v])
-        if current == goal:
-            return gScore, prev
-        open.remove(current)
-        for neighbor,cost in edges[current].items():
-            tentative = gScore[current] + cost
-            if tentative < gScore[neighbor]:
-                prev[neighbor] = current
-                gScore[neighbor] = tentative
-                fScore[neighbor] = tentative + h(neighbor,goal)
-                if neighbor not in open:
-                    open.add(neighbor)
-    return None, None
-
 for i in range(known+1,len(coords)):
-    print(i)
     s = DisjointSet([p for p in product(range(end[0]+1),range(end[1]+1)) if p not in coords[:i]])
     for pos in list(s):
         for dir in DIRS:
             next = tuple_add(pos,dir)
             if valid(next,end) and next not in coords[:i]:
                 s.merge(pos,next)
-    #print(s.subsets())
     if not s.connected(start,end):
         print(coords[i-1])
         break
